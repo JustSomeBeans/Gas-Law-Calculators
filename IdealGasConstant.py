27c50ea3d11ce466c768452dcfcb353016aefc33
JustSomeBeans <102830580+JustSomeBeans@users.noreply.github.com>
2022-05-24 08:09:12 -0700

    Update IdealGasConstant.py

--- IdealGasConstant.py
+++ IdealGasConstant.py
@@ -1,21 +1,21 @@
 def cal(k, pr):
     R = 8.3145
     r = (1 / 8.3145)
-    p = str(input("pressure - "))
+    p = str(input("Input pressure - "))
     if p != "x":
         p = float(p) * pr
-    v = str(input("volume - "))
+    v = str(input("Input volume - "))
     if v != "x":
-         v = float(v)
-    t = str(input("temperature - "))
+        v = float(v)
+    t = str(input("Input temperature - "))
     if t != "x":
         t = float(t) + k
-    n = str(input("mols - "))
+    n = str(input("Input mols - "))
     if n != "x":
         n = float(n)
     def get(w, x, y, z):
         ans = (w * (x * y) / z)
-        print(ans)
+        print("The answer is " + str(ans))
     if p == "x":
         get(R, n, t, v)
     elif v == "x":
@@ -27,14 +27,15 @@ def cal(k, pr):
     srt()
 def srt():
     ans = input("atm or kpa (at/kp) - ")
-    ans2 = input("kelvin or celcius - (c/k) - ")
+    ans2 = input("celcius or kelvin (c/k) - ")
     if ans2 == "c":
         if ans == "kp":
-            cal(273, 1)
+            cal(273.15, 1)
         if ans == "at":
-            cal(273, 101.325)
+            cal(273.15, 101.325)
     if ans2 == "k":
         if ans == "kp":
             cal(0, 1)
+        if ans == "at":
             cal(0, 101.325)
 srt()
